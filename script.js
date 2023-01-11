8721dcfbce980f902b891d0ec48fecb7da5d1faf
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2023-01-11 00:01:29 -0500

    added multiple sections and picked font

---
+++ script.js
@@ -0,0 +1,8 @@
+// const [red, green, blue] = [244, 244, 244]
+// const section1 = document.querySelector('.section1')
+
+// window.addEventListener('scroll', () => {
+//   const y = 1 + (window.scrollY || window.pageYOffset) / 150
+//   const [r, g, b] = [red/(8*y), green/(8*y), blue/(8*y)].map(Math.round)
+//   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+// })
