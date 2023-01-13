61158c38f7a73c63b2ece99fc3bcd2c70399a6cb
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2023-01-13 12:30:24 -0500

    js animations for buttons page 2

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const aboutMeBtn = document.getElementById("about-me");
 const experienceBtn = document.getElementById("experience");
 const contactMeBtn = document.getElementById("contact-me");
-const aboutMeHead = document.getElementById("about-me-header");
+// const aboutMeHead = document.getElementById("about-me-header");
+const educationBtn = document.getElementById("education-btn");
+const interestsBtn = document.getElementById("interests-btn");
+const awardsBtn = document.getElementById("awards-btn");
+
 
 aboutMeBtn.addEventListener("click", function(){
     window.scrollBy(0, window.innerHeight);
+    console.log("clicked")
 });
 
 experienceBtn.addEventListener("click", function(){
@@ -13,4 +18,19 @@ experienceBtn.addEventListener("click", function(){
 
 contactMeBtn.addEventListener("click", function(){
     window.scrollBy(0, 3 * window.innerHeight);
+});
+
+educationBtn.addEventListener("click", function(){
+    educationBtn.classList.toggle("after-click");
+    console.log("clicked")
+});
+
+interestsBtn.addEventListener("click", function(){
+    educationBtn.classList.toggle("after-click");
+    console.log("clicked")
+});
+
+interestsBtn.addEventListener("click", function(){
+    interestsBtn.classList.toggle("after-click");
+    console.log("clicked")
 });
