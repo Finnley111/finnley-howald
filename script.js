9478fb571cbba21d6d9d26d1f00e13f5dc403327
Finnley111 <86789658+Finnley111@users.noreply.github.com>
2023-01-14 16:17:26 -0500

    buttons and changed colour

--- script.js
+++ script.js
@@ -1,63 +1,84 @@
-const aboutMeBtn = document.getElementById("about-me");
-const experienceBtn = document.getElementById("experience");
-const contactMeBtn = document.getElementById("contact-me");
-// const aboutMeHead = document.getElementById("about-me-header");
-const educationBtn = document.getElementById("education-btn");
-const interestsBtn = document.getElementById("interests-btn");
-const awardsBtn = document.getElementById("awards-btn");
-const secondSection = document.getElementById("second-page");
+// const aboutMeBtn = document.getElementById("about-me");
+// const experienceBtn = document.getElementById("experience");
+// const contactMeBtn = document.getElementById("contact-me");
+// // const aboutMeHead = document.getElementById("about-me-header");
+// const educationBtn = document.getElementById("education-btn");
+// const interestsBtn = document.getElementById("interests-btn");
+// const awardsBtn = document.getElementById("awards-btn");
+// const secondSection = document.getElementById("second-page");
 
 
-aboutMeBtn.addEventListener("click", function(){
-    window.scrollBy(0, window.innerHeight);
-    console.log("clicked")
-});
+// aboutMeBtn.addEventListener("click", function(){
+//     window.scrollBy(0, window.innerHeight);
+//     console.log("clicked")
+// });
 
-experienceBtn.addEventListener("click", function(){
-    window.scrollBy(0, 2 * window.innerHeight);
-});
+// experienceBtn.addEventListener("click", function(){
+//     window.scrollBy(0, 2 * window.innerHeight);
+// });
 
-contactMeBtn.addEventListener("click", function(){
-    window.scrollBy(0, 3 * window.innerHeight);
-});
+// contactMeBtn.addEventListener("click", function(){
+//     window.scrollBy(0, 3 * window.innerHeight);
+// });
 
-educationBtn.addEventListener("click", function(e){
-    if (e.target == e.currentTarget) {
-        educationBtn.classList.toggle("after-click");
-        console.log("clicked")
-        if(interestsBtn.classList.contains('after-click')) {
-            interestsBtn.classList.toggle("after-click");
-        }
-        if(awardsBtn.classList.contains('after-click')) {
-            awardsBtn.classList.toggle("after-click");
-        }
-        secondSection.style.backgroundColor = 'green';
-    }
+// educationBtn.addEventListener("click", function(e){
+//     if (e.target == e.currentTarget) {
+//         educationBtn.classList.toggle("after-click");
+//         console.log("clicked")
+//         if(interestsBtn.classList.contains('after-click')) {
+//             interestsBtn.classList.toggle("after-click");
+//         }
+//         if(awardsBtn.classList.contains('after-click')) {
+//             awardsBtn.classList.toggle("after-click");
+//         }
+//         secondSection.style.backgroundColor = 'green';
+//     }
     
-});
+// });
 
-interestsBtn.addEventListener("click", function(e){
-    if (e.target == e.currentTarget) {
-        interestsBtn.classList.toggle("after-click");
-        console.log("clicked")
-        if(educationBtn.classList.contains('after-click')) {
-            educationBtn.classList.toggle("after-click");
-        }
-        if(awardsBtn.classList.contains('after-click')) {
-            awardsBtn.classList.toggle("after-click");
-        }
-    }
-});
+// interestsBtn.addEventListener("click", function(e){
+//     if (e.target == e.currentTarget) {
+//         interestsBtn.classList.toggle("after-click");
+//         console.log("clicked")
+//         if(educationBtn.classList.contains('after-click')) {
+//             educationBtn.classList.toggle("after-click");
+//         }
+//         if(awardsBtn.classList.contains('after-click')) {
+//             awardsBtn.classList.toggle("after-click");
+//         }
+//     }
+// });
 
-awardsBtn.addEventListener("click", function(e){
-    if (e.target == e.currentTarget) {
-        awardsBtn.classList.toggle("after-click");
-        console.log("clicked")
-        if(educationBtn.classList.contains('after-click')) {
-            educationBtn.classList.toggle("after-click");
-        }
-        if(interestsBtn.classList.contains('after-click')) {
-            interestsBtn.classList.toggle("after-click");
-        }
-    }
-});
+// awardsBtn.addEventListener("click", function(e){
+//     if (e.target == e.currentTarget) {
+//         awardsBtn.classList.toggle("after-click");
+//         console.log("clicked")
+//         if(educationBtn.classList.contains('after-click')) {
+//             educationBtn.classList.toggle("after-click");
+//         }
+//         if(interestsBtn.classList.contains('after-click')) {
+//             interestsBtn.classList.toggle("after-click");
+//         }
+//     }
+// });
+
+
+// let width = screen.width;
+// let height = screen.height;
+// const mainContainer = document.querySelector("div")
+
+// mainContainer.addEventListener("resize", function(e){
+//     console.log('hello');
+//     if (width<=height){
+//         mainContainer.classList.toggle("tall-mode");
+        
+//     }
+//     else{
+//         if (mainContainer.classList.contains("tall-mode")){
+//             mainContainer.classList.toggle("tall-mode");
+//             alert('wide');
+//         }
+        
+//         console.log("wide");
+//     }
+// });
